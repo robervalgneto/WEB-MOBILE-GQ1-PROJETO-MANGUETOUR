981514573d8b4160869a936f9c4291cf9f2c205e
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2025-04-10 21:52:24 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles.css';
 import App from './App';
 import Apitarefas from './API/Apitarefas.js';
-import APISTACK from './APISTACK/Api_stack.js';
+import Api_stack from './APISTACK/Api_stack.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
