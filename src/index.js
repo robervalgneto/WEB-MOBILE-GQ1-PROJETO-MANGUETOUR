f538d06028d097e1ac320084a519c51ff02e2d9f
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2025-04-10 21:11:13 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import App from './App';
-
+import apitarefas from './API/apitarefas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <apitarefas />
   </React.StrictMode>
 );
 
